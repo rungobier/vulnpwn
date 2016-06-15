1ed24fe67b707f1bd2521e43c6371351149c4a1c
Vex Woo <hap.ddup@gmail.com>
2016-06-14 21:59:30 -0500

    add excpetions baseclass

---
+++ lib/core/exceptions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+##
+# Current source: https://github.com/open-security/Open-Security-Framework/
+##
+
+
+class FrameworkException(Exception):
+    pass
+
+
+class OptionValidationError(FrameworkException):
+    pass
