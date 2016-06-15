ef396981c4532f66139b68defbb05c94092f2dba
Vex Woo <hap.ddup@gmail.com>
2016-06-15 01:27:22 -0500

    Fix OSX tab completion

--- lib/base/framework.py
+++ lib/base/framework.py
@@ -10,6 +10,16 @@ import os
 import cmd
 import subprocess
 
+# Enable OSX Tab Completion
+# http://stackoverflow.com/questions/7116038/python-tab-completion-mac-osx-10-7-lion
+
+import readline
+import rlcompleter
+if 'libedit' in readline.__doc__:
+    readline.parse_and_bind("bind ^I rl_complete")
+else:
+    readline.parse_and_bind("tab: complete")
+
 
 class Items(dict):
     """Core Options Items"""
